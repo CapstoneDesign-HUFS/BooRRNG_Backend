ea5f6912301f9912b997004bdd43cc333b8785a9
woodzverse <woodzverse@naver.com>
2025-03-24 13:34:00 +0900

    Feature: 회원가입 API 기능구현

--- member/views.py
+++ member/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import SignupSerializer
 
-# Create your views here.
+class SignupView(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'access': str(refresh.access_token),
+                'refresh': str(refresh),
+                'user': {
+                    'id': user.id,
+                    'email': user.email,
+                    'password': user.password,
+                    'nickname': user.nickname,
+                    'birthdate': user.birthdate,
+                    'gender': user.gender,
+                    'agreed_terms': user.agreed_terms
+                }
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
